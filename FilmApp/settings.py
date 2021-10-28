a5bfd02a74e21f86ceb67f535789baa26159f293
borys-zaworowski <borys.zaworowski@gmail.com>
2021-10-28 10:37:14 +0200

    Update settings.py
    Secret key removed

--- FilmApp/settings.py
+++ FilmApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^(+h9i&ff7$o0n&$!x%@vmui%eoi@zk4@&7t)8kk%v=2ye(7+f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
